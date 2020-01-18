dfcd2da7c07c17bd25198565d22516f830543a87
Darxoon <Jakob.priemer@gmail.com>
2020-01-18 12:24:32 +0100

    Moved scripts to scripts folder

---
+++ Assets/_Scripts/Bullet.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private Collider2D _mapCollider;
+
+    private Collider2D _collider;
+
+    private Vector2 _aimAxis;
+    
+    private Rigidbody2D _rigidbody;
+
+    private Collider2D[] _contacts = new Collider2D[20];
+    
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = transform.GetChild(0).gameObject.GetComponent<BoxCollider2D>();
+        _mapCollider = GameManager.Instance.mapCollider;
+    }
+
+    private void OnEnable()
+    {
+        
+        _aimAxis = GameManager.Instance.aimAxis;
+    }
+
+    private void Update()
+    {
+        _rigidbody.velocity = _aimAxis * GameManager.Instance.bulletSpeed;
+//        if (_collider.IsTouchingLayers(LayerMask.GetMask("Enemy")))
+//        {
+//            ContactFilter2D filter = new ContactFilter2D {layerMask = LayerMask.GetMask("Enemy")};
+//            _collider.GetContacts(filter, _contacts);
+//
+//            foreach (Collider2D contact in _contacts)
+//            {
+//                Debug.Log("killled broo", contact);
+//            }
+//        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Map"))
+            gameObject.SetActive(false);
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<Enemy>().Hit(1);
+                
+            gameObject.SetActive(false);
+        }
+    }
+
+}
---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] private float health = 5;
+    
+
+    [Header("Calculations")]
+    
+    [SerializeField] private float turnSpeed;
+    [SerializeField] private float speed;
+    [SerializeField] private float direction;
+
+    [SerializeField] private float explosionStrength;
+    
+    [Header("References")]
+    
+    [SerializeField] private Player player;
+    [SerializeField] private CircleCollider2D killCollider;
+
+    [Header("Components")] 
+    
+    [SerializeField] private new Rigidbody2D rigidbody;
+
+    private Collider2D[] contacts = new Collider2D[50];
+    
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 unitVelocity = (player.transform.position - transform.position).normalized;
+        Quaternion tempDirection = Quaternion.LookRotation(unitVelocity.x > 0 ? -unitVelocity : unitVelocity, Vector3.up);
+        float rawDirection = tempDirection.eulerAngles.x + (unitVelocity.x > 0 ? 180f : 0f);
+        direction = Mathf.LerpAngle(direction, rawDirection, turnSpeed);
+
+        Transform transform1;
+        (transform1 = transform).rotation = Quaternion.Euler(0, 0, direction);
+//        transform1.position += Time.deltaTime * -speed * transform1.right.normalized;
+        rigidbody.velocity = -speed * transform1.right.normalized;
+    }
+
+    public void Hit(float strength)
+    {
+        health -= strength;
+        if (health <= 0.2f)
+            Destroy(gameObject);
+    }
+
+}
