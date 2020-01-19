714b1b95bd8c1cad1359016f5d109d218defc386
Darxoon <Jakob.priemer@gmail.com>
2020-01-19 10:24:22 +0100

    Added OneTimeParticleSystem
    A Script on ParticleSystems that destroys them when they're finished

---
+++ Assets/OneTimeParticleSystem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneTimeParticleSystem : MonoBehaviour
+{
+    [SerializeField] private new ParticleSystem particleSystem;
+
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        particleSystem.Play();
+    }
+
+    private void Update()
+    {
+        if(particleSystem.isStopped)
+            Destroy(gameObject);
+    }
+}
